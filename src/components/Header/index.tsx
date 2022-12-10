7810334e43f2e6205b65f22deee165a785d3675b
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2022-12-10 17:58:48 -0300

    :tada: first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import logo from '../../assets/igniteLogo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logo} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
